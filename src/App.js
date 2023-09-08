c685e80a283f4947c47b410a62f666b033b8b0f1
sthota3 <sreehari.thota@slu.edu>
2023-09-08 16:34:08 -0500

    addition features are added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 // App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
-import SignUp from './pages/signup'; // Import the SignUp component
+import SignUp from './pages/signup'; // Changed the import here
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard'; // Import the Dashboard component
-import Payment from './pages/Payment';
+import Dashboard from './pages/Dashboard';
+import Car from './pages/Car';
+import Bike from './pages/Bike';
+import YourOrders from './pages/YourOrders';
+import OrderHistory from './pages/OrderHistory';
+import Footer from './components/Footer'; // Added the import here
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} /> {/* Add the Login route */}
-          <Route path="/Dashboard" element={<Dashboard />} /> {/* Create a Dashboard route */}
-          <Route path="/payment" element={<Payment />} />
-          {/* Add more routes for different pages */}
-        </Routes>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route path="car" element={<Car />} />
+              <Route path="bike" element={<Bike />} />
+              <Route path="your-orders" element={<YourOrders />} />
+              <Route path="order-history" element={<OrderHistory />} />
+            </Route>
+          </Routes>
+        </div>
+        <Footer /> {/* Added the Footer component here */}
       </div>
     </Router>
   );
---
+++ src/pages/Car.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Car() {
+  return (
+    <div>
+      <h2>Car Page</h2>
+      <p>This is the Car page content.</p>
+    </div>
+  );
+}
+
+export default Car;
